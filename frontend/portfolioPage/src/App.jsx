ccf0a5c7de29822e0fc48f90ccf1ba9e62284b0f
Dawid Ciepko <dciepko17@gmail.com>
2025-03-21 12:04:18 +0100

    Some test changes

--- frontend/portfolioPage/src/App.jsx
+++ frontend/portfolioPage/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Film from "/videos/Film.mp4";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const videoRef = useRef(null);
+  const [speed, setSpeed] = useState(1);
+
+  const changeSpeed = (event) => {
+    const newSpeed = parseFloat(event.target.value);
+    setSpeed(newSpeed);
+    if (videoRef.current) {
+      videoRef.current.playbackRate = newSpeed;
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div></div>
+      <h1>Cześć Agata</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <video ref={videoRef} width="560" controls>
+          <source src={Film} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <label>Prędkość: {speed}x</label>
+        <input
+          type="range"
+          min="0.5"
+          max="3"
+          step="0.1"
+          value={speed}
+          onChange={changeSpeed}
+        />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
